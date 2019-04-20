3603887c54acf4dee3c70279762243abe8203633
ElenaR1 <elena.radoycheva@abv.bg>
2019-04-20 13:10:27 +0300

    Create graphs-recursion.py

---
+++ Week08/graphs-recursion.py
@@ -0,0 +1,34 @@
+def deep_find_bfs2(data, key):
+   found=[]
+   find_el=False
+   #global find_el
+   for k,v in data.items():
+        
+        if k==key:
+            find_el=True
+            return v
+        elif isinstance(v,dict):
+            found.append(v)
+        elif isinstance(v, Iterable) and isinstance(v,dict)==False:
+            found.append(v)
+        print('k:',k,', FOUND: ',found)
+   if find_el==False:
+        for el in found:
+            if isinstance(el,dict):
+                # if deep_find_bfs(el,key)!= None:
+                #         print('in')
+                #         return deep_find_bfs(el,key)
+                for k,v in el.items():
+                     if k==key:
+                        find_el=True
+                        return v
+                     elif isinstance(v,dict):
+                        found.append(v)
+
+
+
+            elif isinstance(el, Iterable) and isinstance(el,dict)==False:
+                for e in el:
+                    if isinstance(e,dict):
+                        if deep_find_bfs(e,key)!= None:
+                            return deep_find_bfs(e,key)
