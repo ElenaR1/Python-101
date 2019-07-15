5634bb289daa92a7fa78304180d90df4afe3b195
ElenaR1 <elena.radoycheva@abv.bg>
2019-07-15 09:19:34 +0300

    Create division.py

---
+++ week01/division.py
@@ -0,0 +1,68 @@
+#Find Factors of Number
+# Python Program to find the factors of a number
+
+# define a function
+def print_factors(x):
+   # This function takes a number and prints the factors
+
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+# change this value for a different result.
+num = 320
+
+# uncomment the following line to take input from the user
+#num = int(input("Enter a number: "))
+
+print_factors(num)
+
+#Check Prime Number
+# Python program to check if the input number is prime or not
+
+num = 407
+
+# take input from the user
+# num = int(input("Enter a number: "))
+
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
+   
+
+#Find all prime factors
+# A function to print all prime factors of  
+# a given number n 
+def primeFactors(n): 
+      
+    # Print the number of two's that divide n 
+    while n % 2 == 0: 
+        print 2, 
+        n = n / 2
+          
+    # n must be odd at this point 
+    # so a skip of 2 ( i = i + 2) can be used 
+    for i in range(3,int(math.sqrt(n))+1,2): 
+          
+        # while i divides n , print i ad divide n 
+        while n % i== 0: 
+            print i, 
+            n = n / i 
+              
+    # Condition if n is a prime 
+    # number greater than 2 
+    if n > 2: 
+        print n 
