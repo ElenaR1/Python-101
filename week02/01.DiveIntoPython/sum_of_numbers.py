18899f417f8db529ce54900cdc64e2b17b34dbee
ElenaR1 <elena.radoycheva@abv.bg>
2019-07-16 16:12:11 +0300

    Create sum_of_numbers.py

---
+++ week02/01.DiveIntoPython/sum_of_numbers.py
@@ -0,0 +1,37 @@
+
+class CustomError(Exception):
+    pass
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+def sum_of_numbers(input_string):
+    temp=''
+    sum=0
+    if hasNumbers(input_string)==False:
+        raise CustomError("no numbers")
+    for index,ch in enumerate(input_string):
+        if ch.isdigit():
+            #print(ch,temp)
+            temp=temp+ch
+        if ch.isdigit()==False:
+            #print(ch)
+            if(temp.isdigit()):
+                #print(ch,temp)
+                temp_num=int(temp)# int('x') ValueError: invalid literal for int() with base 10: 'x'
+                sum=sum+temp_num
+                temp=''
+        if(ch.isdigit and index==len(input_string)-1 and temp.isdigit()):
+             #print("end" ,temp)
+             sum= sum+int(temp)
+    return sum
+
+
+
+
+def main():
+    print(sum_of_numbers("ab125cd3"))
+    print(sum_of_numbers("ab"))
+    
+if __name__=='__main__':
+    main()
