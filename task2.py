2e556bd4ec5bfce0e52a56d439a482caf7ef3fad
ElenaR1 <elena.radoycheva@abv.bg>
2019-07-28 11:29:53 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,10 @@
 import string
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+import csv
+import re
+
+
 def filter(file_name,**kwargs):
     #with open('example_data.csv') as csv_file:
     exclude = set(string.punctuation)
@@ -36,51 +42,84 @@ def filter(file_name,**kwargs):
                     add_element=False           
 
         if add_element==True:
-            el[0] = ''.join(ch for ch in el[0] if ch not in exclude)
-            el[7] = ''.join(ch for ch in el[7] if ch not in exclude)
+            el[0]=''.join(ch for ch in el[0] if ch.isalnum() and ch not in exclude or ch==' ')#for the special characters
+
+            el[7]=''.join(ch for ch in el[7] if ch.isalnum() and ch not in exclude or ch==' ')
+
+            # el[7] = ''.join(ch for ch in el[7] if ch not in exclude)
+            # el[7]=''.join(ch for ch in el[7] if ch.isalnum() or ch==' ')
+
             for ch in el[0]:
                 if ch in numbers.keys():
                     el[0]=el[0].replace(ch,numbers[ch])
             for ch in el[7]:
                 if ch in numbers.keys():
                     el[7]=el[7].replace(ch,numbers[ch])
-                    
+            el[0]=el[0].lower()
+            el[7]=el[7].lower()
+
+            ps = PorterStemmer()         
+            arr_of_snd_part=el[7].split()
+            arr_of_snd_part=[ps.stem(word) for word in arr_of_snd_part]
+            el[7]=' '.join(arr_of_snd_part)
             result_array[len(result_array):]=[[el[0],el[7]]]
         else:
             add_element=True
-    # if 'order_by' in kwargs.keys():
-    #     if kwargs['order_by']=='full_name':
-    #         result_array = sorted(result_array, key=lambda k: k[0])
-    #     if kwargs['order_by']=='favourite_color':
-    #         result_array = sorted(result_array, key=lambda k: k[1])
-    #     if kwargs['order_by']=='company_name':
-    #         result_array = sorted(result_array, key=lambda k: k[2])
-    #     if kwargs['order_by']=='email':
-    #         result_array = sorted(result_array, key=lambda k: k[3])
-    #     if kwargs['order_by']=='phone_number':
-    #         result_array = sorted(result_array, key=lambda k: k[4])
-    #     if kwargs['order_by']=='salary':
-    #         result_array = sorted(result_array, key=lambda k: k[5])
+
 
     l=len(result_array)
+
+    # print(result_array[0:4])
+
+    with open('transformed_records.csv', 'w',encoding='utf-8') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(result_array)
+    csvFile.close()
+
     print(l)
-    for i in range(0,4):
-        print(result_array[i])
-    return result_array[0:4]
+    # for i in range(0,4):
+    #     print(result_array[i])
+    #return result_array[0:4]
 
 
 def main():
-    print('filter')
-    s = "ab1k0"
-    numbers={'0':'zero','1':'one', 2:'two', 3:'three'}
-    print('0' in numbers.keys(),numbers['0'])
-    for el in s:
-        if el in numbers.keys():
-            s=s.replace(el,numbers[el])
-            print(s)
+#     print('filter')
+#     s = "ab1k0"
+#     numbers={'0':'zero','1':'one', 2:'two', 3:'three'}
+#     print('0' in numbers.keys(),numbers['0'])
+#     for el in s:
+#         if el in numbers.keys():
+#             s=s.replace(el,numbers[el])
+#             print(s)
 
-    (filter('records.csv',Language='English'))
+    exclude = set(string.punctuation)
+    # ss="hello? there A-Z-R_T(,**), world, welcome to python.this **should? the next line#followed- by@ an#other %million^ %%like $this."
+    # ss = ''.join(ch for ch in ss if ch not in exclude)
+    # print(ss)
+
+    ps = PorterStemmer() 
+  
+    # a=['ukrnzerozerozerofourfourfourtwotwofour', 'postglacial permian stratigraphy and geography of southern and central africa boundary conditions for climatic modelling']
+    # arr_a=a[1].split()
+    # arr_a=[ps.stem(word) for word in arr_a]
+    # print(arr_a)
+    # a[1]=' '.join(arr_a)
+    # print(a)
    
+
+    filter('records.csv',Language='English')
+    # exclude = set(string.punctuation)
+    # s="embryo-like fossils early metazoans?"
+    # s = ''.join(ch for ch in s if ch not in exclude)
+    # print(s)
+
+    # k='"A theropod–sauropod track assemblage from the ?Middle–Upper Jurassic Shedian Formation at Shuangbai'
+    # print(k)
+    
+    # a=''.join(ch for ch in k if ch.isalnum() and ch not in exclude or ch==' ')
+    # #a=''.join(ch for ch in k if ch not in exclude)
+    # print(a)
+
 
 if __name__=='__main__':
     main()
