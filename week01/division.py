6ec9030c0042f363ad21868093f901372bde4803
ElenaR1 <elena.radoycheva@abv.bg>
2019-10-05 12:53:35 +0300

    Update division.py

--- week01/division.py
+++ week01/division.py
@@ -42,6 +42,30 @@ if num > 1:
 else:
    print(num,"is not a prime number")
    
+ #drug nachin
+# Python program to check if  
+# given number is prime or not 
+  
+num = 11
+  
+# If given number is greater than 1 
+if num > 1: 
+      
+   # Iterate from 2 to n / 2  
+   for i in range(2, num//2): 
+         
+       # If num is divisible by any number between  
+       # 2 and n / 2, it is not prime  
+       if (num % i) == 0: 
+           print(num, "is not a prime number") 
+           break
+   else: 
+       print(num, "is a prime number") 
+  
+else: 
+   print(num, "is not a prime number") 
+
+   
 
 #Find all prime factors
 # A function to print all prime factors of  
