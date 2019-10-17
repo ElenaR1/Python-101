0e449eea38015254ffecceb3442996a732332425
ElenaR1 <elena.radoycheva@abv.bg>
2019-10-17 15:52:18 +0300

    Update things.py

--- things.py
+++ things.py
@@ -169,3 +169,12 @@ def rpn_calculate(expr):
     return z
 
 #print(rpn_calculate("4 8 +"))
+
+ a=[1,2,3,4,5,6,7,8,9,10]
+    print(a[1:])
+    print(a[:3])
+    print(a[-1])
+    print(a[-2])
+    print(a[0:-2])
+    print(a[0:-2:2])
+    print(a[::-2])
